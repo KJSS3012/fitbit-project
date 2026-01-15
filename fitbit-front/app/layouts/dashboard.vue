2f3995b9c293f879fba9ae3ca41d567fd0a81029
Kaique Santos <kaiquesantos.dev@gmail.com>
2026-01-15 14:31:38 -0300

    fix: disable dropdown in DashboardTeamsMenu for consistent behavior

--- fitbit-front/app/layouts/dashboard.vue
+++ fitbit-front/app/layouts/dashboard.vue
@@ -55,7 +55,7 @@ const groups = computed(() => [{
     <UDashboardSidebar id="default" v-model:open="open" collapsible resizable class="bg-elevated/25"
       :ui="{ footer: 'lg:border-t lg:border-default' }">
       <template #header="{ collapsed }">
-        <DashboardTeamsMenu :collapsed="collapsed" :disable-dropdown="route.path.includes('/patients/')" />
+        <DashboardTeamsMenu :collapsed="collapsed" :disable-dropdown="true" />
       </template>
 
       <template #default="{ collapsed }">
